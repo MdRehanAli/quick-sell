e4fa10156f59f3655ab66c7b2927b4c05b5c6de1
MdRehanAli <rehanali21d@gmail.com>
2026-01-16 01:24:06 +0600

    Added items details and card setup

---
+++ src/components/ItemsCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItemsCard = () => {
+    return (
+        <div>
+            <h1>Name: </h1>
+            <p>Price: </p>
+        </div>
+    );
+};
+
+export default ItemsCard;
