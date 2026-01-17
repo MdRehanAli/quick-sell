c22bb0aeedf580f5efe2cc7b6f04e7b7e3f3f657
MdRehanAli <rehanali21d@gmail.com>
2026-01-17 14:00:47 +0600

    Added login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,55 @@
-import React from 'react';
+"use client"
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { FaRegUser } from 'react-icons/fa';
 
 const LoginPage = () => {
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if (email === 'quicksell@gmail.com' && password === 'QuickSell') {
+            document.cookie = "auth=true; path=/";
+            router.push("/items")
+        }
+        else {
+
+        }
+    }
+
+    const handleDemoLogin = (e) => {
+
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
-                    </div>
-                </div>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <form onSubmit={handleLogin} className="card-body text-primary">
+                    <h1 className='text-center text-4xl font-bold text-primary mb-5'>Welcome Back</h1>
+                    <fieldset className="fieldset">
+                        <label className="label text-primary">Email</label>
+                        <input type="email" required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="input w-full"
+                            placeholder="Email" />
+                        <label className="label text-primary">Password</label>
+                        <input
+                            type="password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="input w-full"
+                            placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-primary mt-4">Login</button>
+                    </fieldset>
+                </form>
             </div>
         </div>
     );
